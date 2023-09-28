564e935c75fc43cfd46839047e806366c900d12b
Cachenn <cachann10@mail.ru>
2023-09-28 18:23:01 +0300

    Initial Commit

---
+++ IAmRich1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich1
+//
+//  Created by Ксения Святогорова on 28.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
